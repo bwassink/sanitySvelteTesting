f0a8b87f249ff0b0ea63834823eeb4705c233657
Benjamin Wassink <bwassink@gmail.com>
2023-07-24 19:41:16 -0500

    Change to routing

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -14,7 +14,7 @@
   <nav>
     <ul>
       <li><a data-selected={path === '/'} href="/">Home</a></li>
-      <li><a data-selected={path === '/'} href="/second">Page 2</a></li>
+      <li><a data-selected={path === '/'} href="/routes/second">Page 2</a></li>
     </ul>
   </nav>
 </header>
